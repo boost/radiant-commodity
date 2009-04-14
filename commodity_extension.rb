d79a6060f57b685df860366ff2d25c368918319f
Jeremy Wells <jeremy@boost.co.nz>
2009-04-14 15:01:24 +1200

    Updated comments

--- commodity_extension.rb
+++ commodity_extension.rb
@@ -6,23 +6,14 @@ class CommodityExtension < Radiant::Extension
   description "Provides URLs to grab content without layouts"
   url "http://github.com/boost/radiant-commodity"
   
-  # define_routes do |map|
-  #   map.namespace :admin, :member => { :remove => :get } do |admin|
-  #     admin.resources :snippet_export
-  #   end
-  # end
-
   define_routes do |map|
     map.resources :snippets
     map.connect 'page/*url', :controller => 'pages', :action => 'show'
   end
   
   def activate
-    # admin.tabs.add "Snippet Export", "/admin/snippet_export", :after => "Layouts", :visibility => [:all]
   end
   
   def deactivate
-    # admin.tabs.remove "Snippet Export"
   end
-  
 end
